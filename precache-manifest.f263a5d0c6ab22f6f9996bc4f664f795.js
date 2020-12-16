1b1c8fb03c3b5f28f9f7e8412755ff4a4dee1ff3
Emilia21 <epetkova96@gmail.com>
2020-12-16 22:13:51 +0200

    Updates

---
+++ precache-manifest.f263a5d0c6ab22f6f9996bc4f664f795.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6144ad276f841a2e6bebba89e2d7c8",
+    "url": "/Covid19-Tracker-v2/index.html"
+  },
+  {
+    "revision": "542d14cfd926efac3173",
+    "url": "/Covid19-Tracker-v2/static/css/main.451e7549.chunk.css"
+  },
+  {
+    "revision": "81b9aca941a7f60a64b3",
+    "url": "/Covid19-Tracker-v2/static/js/2.ba72cf15.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19-Tracker-v2/static/js/2.ba72cf15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "542d14cfd926efac3173",
+    "url": "/Covid19-Tracker-v2/static/js/main.5634ee26.chunk.js"
+  },
+  {
+    "revision": "1779f7158dbe92410e7b",
+    "url": "/Covid19-Tracker-v2/static/js/runtime-main.ca7db1f3.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid19-Tracker-v2/static/media/image.d7265326.png"
+  }
+]);
